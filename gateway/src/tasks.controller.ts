3b8e7fc70287387450f4386a0f418255996837ce
Azat Antonyan <azat.antonyan65@gmail.com>
2025-05-19 22:52:02 +0400

    feat: test

--- gateway/src/tasks.controller.ts
+++ gateway/src/tasks.controller.ts
@@ -35,7 +35,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { ConfigService } from './services/config/config.service';
 import { StructuredLogger } from './services/logger';
 // import { File as MulterFile } from 'multer';
-
+import { File } from 'multer';
 const config = new ConfigService()
 
 @Controller('tasks')
@@ -129,7 +129,7 @@ export class TasksController {
   public async createTask(
     @Req() { uuid }: Request & { uuid: string },
     @Body() taskRequest: CreatePostDto,
-    @UploadedFile() file: Express.Multer.File,
+    @UploadedFile() file: File,
   ): Promise<unknown> {
     const storage = new Storage({
       credentials: JSON.parse(config.get('POLLIN_FIREBASE_ADMINSDK_SA'))
@@ -198,7 +198,7 @@ export class TasksController {
     @Req() { uuid }: Request & { uuid: string },
     @Param('id') taskId: string,
     @Body() taskRequest: EditTaskDto,
-    @UploadedFile() file?: Express.Multer.File,
+    @UploadedFile() file?: File,
   ): Promise<unknown> {
     const storage = new Storage({
       credentials: JSON.parse(config.get('POLLIN_FIREBASE_ADMINSDK_SA')),
